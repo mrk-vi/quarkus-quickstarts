7ad1c95d06295a2230e795e93a3daf44026e2b59
Mirko Zizzari <mirko.zizzari@smc.it>
2023-08-01 12:22:47 +0200

    adds Language entity

--- hibernate-reactive-quickstart/src/main/resources/import.sql
+++ hibernate-reactive-quickstart/src/main/resources/import.sql
@@ -1,3 +1,8 @@
 INSERT INTO known_fruits(id, name) VALUES (1, 'Cherry');
 INSERT INTO known_fruits(id, name) VALUES (2, 'Apple');
 INSERT INTO known_fruits(id, name) VALUES (3, 'Banana');
+
+INSERT INTO language(name, "value") VALUES ('English (US)', 'en_US');
+INSERT INTO language(name, "value") VALUES ('English (UK)', 'en_UK');
+INSERT INTO language(name, "value") VALUES ('Español', 'es_ES');
+INSERT INTO language(name, "value") VALUES ('Italiano', 'it_IT');
